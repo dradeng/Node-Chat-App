543bd2a8726f3f01fbc901655819e834891c165b
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-12 14:54:34 -0700

    diff rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,7 +19,14 @@ function scrollToBottom () {
 
 socket.on('connect', function () {
 	console.log('connected to server');
-
+	socket.emit('join', params, function (err) {
+		if (err) {
+			alert(err);
+			window.location.href = '/';
+		} else {
+			console.log('No error');
+		}
+	});
 });
 
 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const socketIO = require('socket.io');
 const http = require('http');
 const { generateMessage, generateLocationMessage } = require('./utils/message');
-
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -18,11 +18,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('new user connected');
 
+	socket.on('join', (params, callback) => {
 
-	socket.emit('newMessage', generateMessage('Admin', 'welcome to chat app'));
+	});
 
-	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+	socket.on('join', (params, callback) => {
+		if (!isRealString(params.name) || !isRealString(params.room)) {
+			callback('Name and room name are required.');
+		}
 
+		server.join(params.room);
+
+
+		socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app'));
+		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+		callback();
+	});
 
 	socket.on('createMessage', (message, callback) => {
 		
