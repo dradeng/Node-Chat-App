c1c58a4086df626531e6531969af5a59afe2737d
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-12 10:43:56 -0700

    add form messages and show messages

--- public/js/index.js
+++ public/js/index.js
@@ -15,11 +15,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
 	console.log('newMessage', message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-	from: 'Frank',
-	text: 'Hi'
-}, function(text) {
-	console.log('Got it', text);
+
+jQuery('#messge-form').on('submit', function (e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
 });
