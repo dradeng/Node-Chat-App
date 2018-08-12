f1e2f101cb56583e93d1d46ab09258788952e5db
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-12 11:44:38 -0700

    css styling

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
 	socket.on('createMessage', (message, callback) => {
 		
 		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback('this is from the server');
+		callback();
 		
 	});
 
